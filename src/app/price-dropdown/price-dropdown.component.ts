83cdbcafac4f13c93b6198175a4a9d761fbd35b9
winter <xwhwt92@hotmail.com>
2017-11-17 17:03:11 +1000

    Good Version

--- src/app/price-dropdown/price-dropdown.component.ts
+++ src/app/price-dropdown/price-dropdown.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PriceDropdownComponent implements OnInit {
 
+  iconPath: string;
   constructor() { }
 
   ngOnInit() {
+    this.iconPath = 'assets/icons/';
   }
 
 }
